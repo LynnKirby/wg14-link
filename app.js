c323cf13341bcf75b709304dca3fd14098757929
Lynn Kirby <lynn@arrak.is>
2020-02-07 02:45:51 -0500

    Refactor error display

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const morgan = require("morgan");
 const path = require("path");
 const process = require("process");
 
+// Load data.
+const redirects = require("./build/redirect.json");
+debug("Loaded data file build/redirect.json");
+
+//
+// General app configuration.
+//
+
 const app = express();
+module.exports = app;
 
 app.disable("x-powered-by");
 
@@ -18,29 +27,12 @@ app.disable("x-powered-by");
 // one used by both Express and serve-static so it will change everything.
 require("send").mime.define({ "text/plain": ["yml", "yaml"] }, true);
 
-const documentNotFoundError = () =>
-  createError(404, "page not found", { expose: true });
-const unassignedIdError = () =>
-  createError(404, "unassigned document ID", { expose: true });
-const missingDocumentLinkError = () =>
-  createError("404", "document file missing", {
-    expose: true,
-    description:
-      "the document ID you provided is valid but we do not have a link " +
-      "to the document file. it has probably been lost to the sands of " +
-      "time...",
-  });
-
 // We use both checks in case NODE_ENV is not set because, for example, we only
 // want to show details on error pages when it's "development" but not unknown,
 // which may mean we're actually deployed to production but accidentally didn't
 // set the environment variable.
 const isProduction = process.env.NODE_ENV === "production";
 const isDevelopment = process.env.NODE_ENV === "development";
-
-// Load data.
-const redirects = require("./build/redirect.json");
-debug("Loaded data file build/redirect.json");
 
 // Set EJS as the view engine.
 app.set("views", "./views");
@@ -53,6 +45,78 @@ if (isProduction) {
 } else {
   app.use(morgan("dev"));
 }
+
+//
+// Error helpers.
+//
+
+const makeRenderTemplateError = (tmpl, contentType) => (err, res) => {
+  const status = err.status || 500;
+
+  if (err.expose || isDevelopment) {
+    res.locals.message = err.message;
+    res.locals.description = err.description;
+  } else if (status === 500) {
+    res.locals.message = "internal server error";
+    res.locals.description = null;
+  } else {
+    res.locals.message = "unknown error";
+    res.locals.description = null;
+  }
+
+  res.locals.status = status;
+  res.locals.stack = isDevelopment ? err.stack : null;
+
+  res.header("Content-Type", contentType);
+  res.status(status);
+  res.render(tmpl);
+};
+
+const makeRenderJsonError = contentType => (err, res) => {
+  const status = err.status || 500;
+  const result = {};
+
+  if (err.expose || isDevelopment) {
+    result.error = `${err.message} - ${err.description}`;
+  } else if (status === 500) {
+    result.error = "internal server error";
+  } else {
+    result.error = "unknown error";
+  }
+
+  result.stack = isDevelopment ? err.stack.split("\n") : null;
+
+  res.status(status);
+  res.header("Content-Type", contentType);
+  res.send(JSON.stringify(result, null, 2));
+};
+
+const renderHtmlError = makeRenderTemplateError("error-html", "text/html");
+const renderPlainTextError = makeRenderTemplateError(
+  "error-plain",
+  "text/plain"
+);
+const renderJsonError = makeRenderJsonError("application/json");
+const renderYamlError = makeRenderJsonError("text/plain");
+
+const documentNotFoundError = () =>
+  createError(404, "page not found", { expose: true });
+
+const unassignedIdError = () =>
+  createError(404, "unassigned document ID", { expose: true });
+
+const missingDocumentLinkError = () =>
+  createError("404", "document file missing", {
+    expose: true,
+    description:
+      "the document ID you provided is valid but we do not have a link " +
+      "to the document file. it has probably been lost to the sands of " +
+      "time...",
+  });
+
+//
+// Handlers.
+//
 
 // Host static files from `public` and `build/public`.
 app.use(express.static(path.join(__dirname, "public")));
@@ -101,27 +165,25 @@ app.use((req, res, next) => {
   next(documentNotFoundError());
 });
 
-// Error handler (must have four parameters).
+// Default error handler (must have four parameters).
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  const status = err.status || 500;
+  const segments = req.path.split(".");
+  const extension = segments.length == 1 ? "" : segments[segments.length - 1];
 
-  if (err.expose || isDevelopment) {
-    res.locals.message = err.message;
-    res.locals.description = err.description;
-  } else if (status === 500) {
-    res.locals.message = "internal server error";
-    res.locals.description = null;
-  } else {
-    res.locals.message = "unknown error";
-    res.locals.description = null;
+  switch (extension) {
+    case "yml":
+    case "yaml":
+      renderYamlError(err, res);
+      break;
+    case "json":
+      renderJsonError(err, res);
+      break;
+    case "bibtex":
+      renderPlainTextError(err, res);
+      break;
+    default:
+      renderHtmlError(err, res);
+      break;
   }
-
-  res.locals.status = status;
-  res.locals.stack = isDevelopment ? err.stack : null;
-
-  res.status(status);
-  res.render("error");
 });
-
-module.exports = app;
